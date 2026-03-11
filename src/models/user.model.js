e0ca930876199a30d1bcc956d800baea0e20e3a5
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-03-11 21:06:39 +0530

    feat: add jsonwebtoken for token generation and update user model with access and refresh token methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
     
@@ -25,7 +26,9 @@ const userSchema = new mongoose.Schema({
 
     lastLoginAt: {
         type: Date
-    }
+    },
+
+    refreshToken: String
 
 });
 
@@ -34,8 +37,24 @@ userSchema.pre("save", async function() {
     this.passwordHash = await bcrypt.hash(this.passwordHash, 10);
 })
 
-userSchema.methods.isPasswordCorrect = async function(password) {
+userSchema.methods.isPasswordCorrect = async function(password) {   
     return await bcrypt.compare(password, this.passwordHash);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        displayName: this.displayName
+    }, process.env.ACCESS_TOKEN_SECRET, 
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+    }, process.env.REFRESH_TOKEN_SECRET, 
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
 }
 
 export const User = mongoose.model("User", userSchema);

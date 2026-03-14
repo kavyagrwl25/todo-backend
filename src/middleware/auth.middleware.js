23123bd769b65699e6814ea204b63977e71dc70f
kavyagrwl25 <kavyaagarwal8273@gmail.com>
2026-03-15 02:08:50 +0530

    feat: add authentication middleware for access token handling and route protection

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,6 @@
+
+
+// i need a header to know which type of authZ user has
+// i will be doing this through access token
+// based on authZ, i will protect routes from unauthorized users
+// once the access token expires, i will refresh them through refresh token
